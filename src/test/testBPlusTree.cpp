7d2df82e52b13dab5a961622650b61c6abc21034
ChenMouxiang <cmx_1007@foxmail.com>
2019-06-18 19:30:48 +0800

    [MERGE] RecordManager & CatalogManager

---
+++ src/test/testBPlusTree.cpp
@@ -0,0 +1,92 @@
+#include "BPlusTree.h"
+#include <cstdlib>
+#include <ctime>
+#define SIZE 1000
+#define NodeSize 4096
+
+INode<float, NodeSize> nodes[SIZE];
+float elements[SIZE];
+int top = 2;
+
+void shuffle() {
+	for (int i = 0; i < SIZE - 1; i++) {
+		for (int j = i + 1; j < SIZE; j++) {
+			if (rand() % 2) {
+				float t = elements[i];
+				elements[i] = elements[j];
+				elements[j] = t;
+			}
+		}
+	}
+}
+
+int main() {
+	srand(time(NULL));
+	
+	for (int i = 0; i < SIZE; i++) {
+		nodes[i].id = i;
+		nodes[i].contentSize = 0;
+		nodes[i].isLeaf = true;
+		elements[i] = i;
+	}
+	
+	BPlusTree<float, NodeSize> tree(&nodes[1], [](int id) {
+		return &nodes[id];
+	}, [](auto node) {
+		nodes[node->id] = *node;
+	}, [](auto node) {
+		
+	}, []() {
+		return &nodes[top++];
+	});
+	// test insert
+	shuffle();
+	for (int i = 0; i < SIZE; i++) {
+		tree.insert(elements[i], -1);
+	}
+	// test find
+	shuffle();
+	for (int i = 0; i < SIZE; i++) {
+		auto re = tree.find(elements[i]);
+		if (re->node->value[re->position] != elements[i]) {
+			std::cout << "error!!!: " << i << std::endl;
+		}
+	}
+	// test find min/max
+	auto min = tree.findExtreme(false);
+	if (min->node->value[min->position] != 0) {
+		std::cout << "error!!!: min = " << min->node->value[min->position] << std::endl;
+	}
+	auto max = tree.findExtreme(true);
+	if (max->node->value[max->position] != SIZE - 1) {
+		std::cout << "error!!!: max = " << max->node->value[max->position] << std::endl;
+	}
+	tree.printTree();
+	// test iteration
+	auto current = min->node;
+	do {
+		std::cout << current->value[0] << " ";
+		current = tree.next(current);
+	} while (current != max->node && current->id > 0);
+	std::cout << std::endl;
+	tree.printTree();
+	// test delete
+	shuffle();
+	int i, j;
+	for (i = 0; i < SIZE / 2; i++) {
+		tree.del(elements[i]);
+	}
+	tree.printTree();
+	for (j = i + 1; j < SIZE; j++) {
+		auto re = tree.find(elements[j]);
+		if (re->node->value[re->position] != elements[j]) {
+			std::cout << "error!!!: " << j << std::endl;
+		}
+	}
+	for (; i < SIZE - 1; i++) {
+		tree.del(elements[i]);
+	}
+	tree.printTree();
+	return 0;
+}
+
---
+++ src/test/testIndexManager.cpp
@@ -0,0 +1,70 @@
+#include "IndexManager.h"
+#include "string"
+
+using namespace std;
+
+void generateString(char* data, int i)
+{
+	int j = i, k = 0;;
+	while (j > 0) {
+		data[k++] = j % 26 + 'a';
+		j /= 26;
+	}
+	data[k] = '\0';
+}
+int main()
+{
+	string indexName = "indexname";
+	string tableName = "tableName";
+	string keyName = "a";
+	Index index(indexName, tableName, keyName);
+	BufferManager bufferManager;
+	if (bufferManager.JudgeFileExistence(indexName + ".index")) {
+		IndexManager::dropIndex(index);
+	}
+	
+	IndexManager::createNewIndex(index);
+	IndexManager manager(index);
+	Type type(BaseType::INT, 7);
+	// test add
+	for (int i = 0; i < 10000; i++) {
+		int j = i;
+		//char data[7];
+		//generateString(data, i);
+		Value value(type, &j);
+		manager.insertEntry(&value, i + 1000);
+	}
+	manager.printTree();
+	// test delete
+	cout << "begin delete: ";
+	for (int i = 0; i < 520; i += 2) {
+		int j = i;
+		Value value(type, &j);
+		manager.deleteEntry(&value);
+	}
+	// test disk
+	cout << "begin fetch from disk: ";
+	IndexManager manager2(index);
+	manager2.printTree();
+	// test find
+	cout << "begin find" << endl;
+	char data[7];
+	generateString(data, 100);
+	Value value(type, data);
+	auto iter = manager2.find(&value);
+	while (iter) {
+		cout << iter->currentValue->getAsType<int>() << ": " << iter->indexInRecord << endl;
+		iter = iter->next();
+	}
+	// test iterator
+	cout << "begin iteration" << endl;
+	int count = 0;
+	iter = manager2.findMinOrMax(true);
+	while (iter) {
+		cout << iter->currentValue->getAsType<int>() << ": " << iter->indexInRecord << endl;
+		iter = iter->next();
+		count++;
+	}
+	cout << count;
+
+}
