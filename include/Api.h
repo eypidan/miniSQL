a0279b8e1fbd3c7a805a39f53e369d8fc14d2ba3
ChenMouxiang <cmx_1007@foxmail.com>
2019-06-19 23:05:49 +0800

    [UPDATE] API annotation

--- include/Api.h
+++ include/Api.h
@@ -3,10 +3,13 @@
 #include <MetaData.h>
 
 namespace API {
+	/**
+	 * template struct of SQL result returned by API functions.
+	 */
 	template <typename T>
 	struct SQLResult {
-		std::shared_ptr<T> result;
-		clock_t durationMS;
+		std::shared_ptr<T> result; // result
+		clock_t durationMS; // time period in millisecond
 		bool isSuccess = true;
 		const std::string errorMessage;
 
@@ -14,25 +17,44 @@ namespace API {
 			: result(result), durationMS(durationMS), isSuccess(isSuccess), errorMessage(errorMessage) {}
 	};
 
+	
 	SQLResult<void> createTable(Table &table);
 
-	SQLResult<void> dropTable(std::string & tableName); // tableName only
+	SQLResult<void> dropTable(std::string & tableName);
 
 	SQLResult<void> createIndex(Index &index);
 
-	SQLResult<void> dropIndex(std::string & indexName); // indexName only
+	SQLResult<void> dropIndex(std::string & indexName);
 
+	/**
+	 * Do something like "select [properties] from [tableName] where [predicates]"
+	 * @param properties: projection properties
+	 * @param tableName: from clause
+	 * @param predicates: where clauses linked by "and"
+	 * @return: select results and table specified
+	 */
 	SQLResult<std::pair<View, Table>> select(
 		std::vector<std::string> &properties,
 		std::string &tableName,
 		std::vector<Predicate> &predicates
 	);
 
+	/**
+	 * Do something like "insert into [tableName] values ([values])"
+	 * @param tableName
+	 * @param values: record
+	 */
 	SQLResult<void> insert(
 		std::string &tableName,
 		std::vector<Value> &values
 	);
 
+	/**
+	 * Do something like "delete from [tableName] where [predicates]"
+	 * @param tableName: from clause
+	 * @param predicates: where clauses linked by "and"
+	 * @return: the total number of deleted records
+	 */
 	SQLResult<int> deleteFrom(
 		std::string &tableName,
 		std::vector<Predicate> &predicates
